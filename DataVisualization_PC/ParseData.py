396a1e1d7513e905afb106198a0acd5e8aab45fc
eddylim95 <eddylim95@hotmail.com>
2018-10-16 21:47:27 +0800

    Add graphs to be adaptive to different file types with same format

--- DataVisualization_PC/ParseData.py
+++
@@ -1,6 +0,0 @@
-def parseData(stream):
-    streamList = stream[3:].replace("/t", " ").split()
-    # streamList[0] = int(streamList[0], 16)
-    for i in range(1, len(streamList)-1):
-        streamList[i] = float(streamList[i])
-    return streamList
--- DataVisualization_PC/PlotData.py
+++ DataVisualization_PC/PlotData.py
@@ -1,32 +1,34 @@
 import numpy as np
 from matplotlib import animation
 import matplotlib.pyplot as plt
-from MockDataSource import readData
+from Data import getData
+from MockDataSource import readStreamData
 import time
+import sys
 
-# Setup plot settings
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-xdata, ydata = [], []
+def viewGraph(fileType):
+    fileTypeData = getData()[fileType]
+    # Setup plot settings. Excludes Millis as a plot
+    fig, ax = plt.subplots(len(fileTypeData) - 1, 1, sharex= True)
+    ani = animation.FuncAnimation(fig, _animate, fargs=(ax, fileType, fileTypeData, readStreamData), interval = 1000)
 
-def animate(i, xdata, ydata):
-    headers = readData()
-    xdata.append(headers["Millis"])
-    ydata.append(headers["V_cI"])
+    plt.show()
 
-    # Limit x and y lists to 20 items
-    xdata = xdata[-20:]
-    ydata = ydata[-20:]
-
-    ax.clear()
-    ax.plot(xdata, ydata)
+def _animate(i, ax, fileType, fileTypeData, readStreamData):
+    # call readSteamData function
+    readStreamData()
+    # Setup plot for each data
+    i = 0
+    for header in fileTypeData:
+        if header == "Millis":
+            continue
+        ax[i].clear()
+        ax[i].plot(fileTypeData["Millis"], fileTypeData[header])
+        ax[i].set_ylabel(header)
+        i += 1
 
     # Format plot
     plt.xticks(rotation=45, ha='right')
     plt.subplots_adjust(bottom=0.30)
-    plt.title('Log')
-    plt.ylabel('V_cI')
 
-ani = animation.FuncAnimation(fig, animate, fargs=(xdata, ydata), interval = 200)
-
-plt.show()
+viewGraph("CS")
