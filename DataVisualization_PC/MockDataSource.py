3f576e9613a3183b2f5d73dcbae20bac07a3717c
eddylim95 <eddylim95@hotmail.com>
2018-10-16 21:46:28 +0800

    Refactor code

--- DataVisualization_PC/MockDataSource.py
+++ DataVisualization_PC/MockDataSource.py
@@ -1,26 +1,10 @@
 from random import randint
-from ParseData import parseData
+from Data import updateData, getData
 
-headers = {
-    "Millis": 0,
-    "V_cI": 0,
-    "A_cI": 0,
-    "V_cO": 0,
-    "A_cO": 0,
-    "V_MT": 0,
-    "A_MT": 0
-    }
-
-def readData():
+def readStreamData():
     with open("SampleLogs/CS.TXT") as f:
         # remove header row
         lines = f.readlines()[1:]
         randomLine = lines[randint(0, len(lines)-1)]
-        dataList = parseData(randomLine)
 
-        j = 0
-        for item in headers:
-            headers[item] = dataList[j]
-            j += 1
-
-        return headers
+        updateData(randomLine)
