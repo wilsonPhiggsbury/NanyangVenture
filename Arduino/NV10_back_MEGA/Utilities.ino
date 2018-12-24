1ae673b01c0da76674a3e181032dfbf8f1f30fbf
wilsonPhiggsbury <mingxiugoh@gmail.com>
2018-12-25 01:10:30 +0800

    Fixed SD card crashing issue. Is memory leak that cannot be bypassed using file.close() method.

--- Arduino/NV10_back_MEGA/Utilities.ino
+++ Arduino/NV10_back_MEGA/Utilities.ino
@@ -1,9 +1,18 @@
-
+#include <EEPROM.h>
 bool initSD(char* path)
 {
+	EEPROM.begin();
 	const uint8_t FILENAME_DIGITS = 4;
+	char index[FILENAME_DIGITS + 1];
 	if (!SD.begin(SD_SPI_CS_PIN))
 	{
+		return false;
+	}
+	// skip looking at SD card if it is too full.
+	if (EEPROM.read(1) == 1)
+	{
+		EEPROM.write(1, 0);
+		Serial.println(F("NOT logging into SD card."));
 		return false;
 	}
 	strcpy(path, "/");
@@ -12,7 +21,7 @@ bool initSD(char* path)
 
 	// Comb through existing files in SD card to obtain the latest index. Use it to name our new folder.
 	File f = SD.open("/");
-	File sub;
+	File sub, sub2;
 	int existingIndex = 0; // start from index 0
 	while (sub = f.openNextFile())
 	{
@@ -22,9 +31,57 @@ bool initSD(char* path)
 			existingIndex = max(thisIndex, existingIndex);
 		}
 	}
+	if (existingIndex >= 30)
+	{
+		Serial.println(F("SD card is nearing threshold to crash Arduino. Wipe the card? (y/n)"));
+		while (!Serial.available());
+		delay(100);
+		char response = Serial.read();
+		while (Serial.available())
+			Serial.read();
+		if (response == 'y')
+		{
+			Serial.println(F("Wiping card..."));
+			for (int i = 1; i < 50; i++)
+			{
+				sprintf(index, "%04d", i);
+				strcpy(path + 5, index);
+				strcat(path, "/");
+				Serial.println(path);
 
+				sub = SD.open(path);
+				while (sub2 = sub.openNextFile(FILE_WRITE))
+				{
+					Serial.print("\t");
+					Serial.println(sub2.name());
+					strcat(path, sub2.name());
+					SD.remove(path);
+					strcpy(path + FILENAME_HEADER_LENGTH, "");
+					sub2.close();
+				}
+				sub.close();
+				SD.rmdir(path);
+
+				delay(50);
+			}
+			Serial.println(F("Card wiped. Resetting in..."));
+			delay(1000);
+			for (int i = 5; i >= 0; i--)
+			{
+				Serial.print(i);
+				Serial.print("...");
+				delay(1000);
+			}
+		}
+		else
+		{
+			// tell arduino to ignore sd card on next reset.
+			EEPROM.write(1, 1);
+		}
+		// reset the arduino
+		asm volatile ("  jmp 0");
+	}
 	// pad '0' on the front if number contains less than 4 digits
-	char index[FILENAME_DIGITS + 1];
 	sprintf(index, "%04d", existingIndex + 1);
 	//uint8_t paddingZeroCounter = FILENAME_DIGITS;
 	//int existingIndex_tmp = existingIndex + 1;
@@ -43,7 +100,8 @@ bool initSD(char* path)
 	sub.close();
 
 	// A new index number is assigned to *path! e.g. /LOG_0002
-	strcat(path, index);
+	strcpy(path + 5, index);
+	Serial.print("Logging into folder ");	Serial.println(path);
 	SD.mkdir(path);
 	// initialize the interior folder structure
 	strcat(path, "/");
