2209afb98fca89f86525de08d38f1d10ac2c30e8
wilsonPhiggsbury <mingxiugoh@gmail.com>
2019-02-15 15:42:45 +0800

    Relocate CANSerializer

---
+++ Arduino/libraries/CANSerializer/CANSerializer.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		CANSerializer.cpp
+ Created:	2/15/2019 3:38:38 PM
+ Author:	MX
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "CANSerializer.h"
+
+
---
+++ Arduino/libraries/CANSerializer/CANSerializer.h
@@ -0,0 +1,19 @@
+/*
+ Name:		CANSerializer.h
+ Created:	2/15/2019 3:38:38 PM
+ Author:	MX
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _CANSerializer_h
+#define _CANSerializer_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
