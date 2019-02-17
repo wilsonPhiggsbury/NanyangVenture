ef33633336daf353da753fb5a5d125a1bd9122b1
wilsonPhiggsbury <mingxiugoh@gmail.com>
2019-02-18 00:50:57 +0800

    Fixed some merge errors

--- Arduino/libraries/CANSerializer/CANSerializer.h
+++ Arduino/libraries/CANSerializer/CANSerializer.h
@@ -5,15 +5,33 @@
  Editor:	http://www.visualmicro.com
 */
 
-#ifndef _CANSerializer_h
-#define _CANSerializer_h
+#ifndef _CANSERIALIZER_h
+#define _CANSERIALIZER_h
 
 #if defined(ARDUINO) && ARDUINO >= 100
-	#include "arduino.h"
+#include "arduino.h"
 #else
-	#include "WProgram.h"
+#include "WProgram.h"
 #endif
 
+#include <mcp_can.h>
+class CANFrame
+{
+public:
+	unsigned long id;
+	byte length;
+	byte payload[8]; // TODO: extend payload byte length, then perform slicing in CANSerializer
+};
+class CANSerializer
+{
+private:
+	MCP_CAN* CAN;
+	//DataPoint* dataPoints[1]; // TODO: implement convertion to raw CAN bytes only inside CANSerializer
+public:
+	bool init(uint8_t cs);
+	bool sendCanFrame(CANFrame*);
+	// Populates Packet only if data is available.
+	bool receiveCanFrame(CANFrame*);
+};
 
 #endif
-
