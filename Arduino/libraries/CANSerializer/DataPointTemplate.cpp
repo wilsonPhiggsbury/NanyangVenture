57dc32998633c26cf8c90d305907c9f499560b4d
wilsonPhiggsbury <mingxiugoh@gmail.com>
2019-10-20 05:43:16 +0800

    Better data point template documentation

--- Arduino/libraries/CANSerializer/DataPointTemplate.cpp
+++ Arduino/libraries/CANSerializer/DataPointTemplate.cpp
@@ -2,9 +2,8 @@
 // 
 // 
 #include "DataPointTemplate.h"
-// parameter(CANbytes, stringChars)
-// param1(2,4), param2(1,2)
-DataPointTemplate::DataPointTemplate(uint8_t CANId) :DataPoint("XX", CANId, 8)
+// ## Please configure your 2-char String header, CAN ID and total parameter bytes
+DataPointTemplate::DataPointTemplate() :DataPoint("XX", 0xFF, 8)
 {
 }
 
@@ -14,32 +13,65 @@ void DataPointTemplate::insertData(uint32_t param1, uint32_t param2)
 	this->param2 = param2;
 }
 
+void DataPointTemplate::insertData(uint8_t param1, float param2, float param3, float param4)
+{
+	//## param 1 has expected range [0, 255]
+	//## param 2 has expected range [0, 1] with 1 decimal precision
+	//## param 3 has expected range [-100, 100] with 1 decimal precision
+	//## param 4 is represented by float (yields maximum precision system can provide)
+	this->param1 = param1; // passing in uint8_t, directly assign
+	this->param2 = param2*10; // passing in float, convert to internal representation (param2*10 must be in range [0, 255])
+	this->param3 = (param3 + 100) * 10; // passing in float, convert to internal representation ((param3+100)*10) must be in range [0, 65535])
+	this->param4 = (uint32_t)param4; // passing in float, directly convert 4-byte data into 4-byte data
+
+	// these can be implemented as seperate setter functions as well
+}
+//## getter function must tally
 uint16_t DataPointTemplate::getParam1()
 {
 	return param1;
 }
-
+//## getter function must tally
 float DataPointTemplate::getParam2()
 {
-	return param2;
+	return param2 / 10.0;
 }
-
+//## getter function must tally
+float DataPointTemplate::getParam3()
+{
+	return (param3 / 10.0) - 100;
+}
+//## getter function must tally
+float DataPointTemplate::getParam4()
+{
+	return (float)param4;
+}
 void DataPointTemplate::packString(char *str)
 {
+	// do not touch this line
 	char* shiftedStr = DataPoint::packStringDefault(str);
-	// param1 = 4, param2 = 2
-	sprintf(shiftedStr, "%04d\t%02d", param1, param2);
-}
 
+	//## how do you want to visualize your data as string?
+#ifdef __AVR__
+	char floatStringBuffer[8];
+	dtostrf(param2 / 10.0, 3, 1, floatStringBuffer);
+	sprintf(shiftedStr, "%d\t%s", param1, floatStringBuffer); // note: sprintf("%f") does not work on AVR. This is a workaround.
+#elif defined _SAM3XA_
+	sprintf(shiftedStr, "%d\t%4.1f", param1, param2);
+#endif
+}
 void DataPointTemplate::unpackString(char * str)
 {
+	// do not touch these lines
 	char* ptr = strtok(str, "\t");
 	ptr = strtok(NULL, "\t");
 	timeStamp = strtoul(ptr, NULL, 16);
 
+	//## how do you undo your packing, and restore the params from the packed string?
 	ptr = strtok(NULL, "\t");
 	param1 = atoi(ptr);
 
 	ptr = strtok(NULL, "\t");
 	param2 = strtod(ptr, NULL);
+
 }
--- Arduino/libraries/CANSerializer/DataPointTemplate.h
+++ Arduino/libraries/CANSerializer/DataPointTemplate.h
@@ -4,18 +4,36 @@
 #define _DATAPOINTTEMPLATE_h
 
 #include <DataPoint.h>
+
+/* fields to change are marked with "##"
+
+	demo field:
+		XX	XX	XX	XX	XX	XX	XX	XX
+		p1	p2	p3	p3	p4	p4	p4	p4
+	1B	0	1	2	3	4	5	6	7
+	2B	0	-	1	-	2	-	3	-
+	4B	0	-	-	-	1	-	-	-
+		^								param 1 starts here, at 1B[0]
+			^							param 2 starts here, at 1B[1]
+				^						param 3 starts here, at 2B[1]
+						^				param 4 starts here, at 4B[1]
+*/
 class DataPointTemplate:public DataPoint
 {
  protected:
-	 uint32_t& param1 = data.Long[0];
-	 float& param2 = data.Float[1]; // params can be higher resolution than their packed variants (this float is packed as uint16_t in .cpp file)
+	 uint8_t& param1 = data.Byte[0]; //## is example param that has expected range [0, 255]
+	 uint8_t& param2 = data.Byte[1]; //## is example param that has expected range [0, 1] with 1 decimal precision
+	 uint16_t& param3 = data.UInt[1];//## is example param that has expected range [-100, 100] with 1 decimal precision
+	 uint32_t& param4 = data.Long[1];//## is example param represented by float (yields maximum precision system can provide)
 
  public:
-	DataPointTemplate(uint8_t CANId);
+	DataPointTemplate();
 
-	void insertData(uint32_t param1, uint32_t param2);
+	void insertData(uint8_t param1, float param2, float param3, float param4);
 	uint16_t getParam1();
 	float getParam2();
+	float getParam3();
+	float getParam4();
 	void packString(char*);
 	void unpackString(char * str);
 };
